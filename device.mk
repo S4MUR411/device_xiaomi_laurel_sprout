9da87025501a303f087e55c43d12dd938a7471dc
Himanish Mandrekar <mandrekarh25@gmail.com>
2022-01-27 03:15:45 +0530

    laurel_sprout: Add hotword support

--- device.mk
+++ device.mk
@@ -138,6 +138,10 @@ PRODUCT_PACKAGES += \
     libhidltransport \
     libhwbinder
 
+# Hardware
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.freeform_window_management.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.software.freeform_window_management.xml
+
 # Init
 PRODUCT_PACKAGES += \
     init.recovery.qcom.rc
